af15e8f6eedda5f45c67acbe4450951d97b3fa48
Martina <Martina@ZVIJER>
2016-10-20 17:34:17 +0200

    Model made

---
+++ LoginApp.Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Model
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string role { get; set; }
+        public string avatarUrl { get; set; }
+        public string passHash { get; set; }
+    }
+}
---
+++ LoginApp.Model/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Model
+{
+    public class UserRepository
+    {
+        public List<User> users { get; set; }
+    }
+}
